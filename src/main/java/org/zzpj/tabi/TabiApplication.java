12adb89c2344ac5139b1c326455621916997961f
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-03-27 22:13:17 +0100

    Add init project structure

---
+++ src/main/java/org/zzpj/tabi/TabiApplication.java
@@ -0,0 +1,13 @@
+package org.zzpj.tabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zzpj/tabi/TabiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zzpj.tabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
