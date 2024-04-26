bab15e99ed6db6051ece6cac2001ed9ee081e9a9
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-04-27 00:52:49 +0200

    Add an SQL script for populating the DB

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO client (id, name, email) VALUES
+    ('00000000-0000-0000-0000-000000000001', 'Foo', 'foo@email.com'),
+    ('00000000-0000-0000-0000-000000000002', 'Bar', 'bar@email.com'),
+    ('00000000-0000-0000-0000-000000000003', 'Baz', 'baz@email.com');
