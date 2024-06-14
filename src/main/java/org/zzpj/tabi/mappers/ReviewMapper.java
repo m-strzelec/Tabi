605d97db9648f15d29746d0f4fe9ca1e888c6002
Marcin Strzelec <m.strzelec21052003@gmail.com>
2024-06-15 00:29:22 +0200

    Remove null check from `ReviewMapper`

--- src/main/java/org/zzpj/tabi/mappers/ReviewMapper.java
+++ src/main/java/org/zzpj/tabi/mappers/ReviewMapper.java
@@ -5,14 +5,9 @@ import org.zzpj.tabi.entities.Review;
 
 public class ReviewMapper {
     static public ReviewDTO toReviewDTO(Review review) {
-        if (review != null) {
-            return new ReviewDTO(review.getRating(),
-                    review.getComment(),
-                    review.getTravel().getId(),
-                    review.getClient().getId());
-        } else {
-            // TODO: Throw exception
-            return null;
-        }
+        return new ReviewDTO(review.getRating(),
+                review.getComment(),
+                review.getTravel().getId(),
+                review.getClient().getId());
     }
 }
