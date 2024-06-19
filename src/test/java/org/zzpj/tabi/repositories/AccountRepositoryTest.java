235aa6942e0c437d7d58985435d8e6c17e76dd86
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-06-19 02:52:13 +0200

    Add some test cases

--- src/test/java/org/zzpj/tabi/repositories/AccountRepositoryTest.java
+++ src/test/java/org/zzpj/tabi/repositories/AccountRepositoryTest.java
@@ -1,29 +1,74 @@
 package org.zzpj.tabi.repositories;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.zzpj.tabi.entities.Account;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Optional;
 
 @Testcontainers
 @SpringBootTest
 public class AccountRepositoryTest {
 
     @Container
-    @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgres::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgres::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Autowired
     AccountRepository accountRepository;
+
+    @BeforeAll
+    static void setUp() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgres.stop();
+    }
 
     @Test
     void connectionEstablished() {
         assertThat(postgres.isCreated()).isTrue();
         assertThat(postgres.isRunning()).isTrue();
+    }
+
+    @Test
+    void testFindByLogin() {
+        String login = "foo";
+        Optional<Account> foo = accountRepository.findByLogin(login);
+
+        assertThat(foo.isPresent()).isTrue();
+        assertThat(foo.orElseThrow().getLogin().equals(login)).isTrue();
+    }
+
+    @Test
+    void testFindByEmail() {
+        String email = "foo@email.com";
+        Optional<Account> foo = accountRepository.findByEmail(email);
+
+        assertThat(foo.isPresent()).isTrue();
+        assertThat(foo.orElseThrow().getEmail().equals(email)).isTrue();
+    }
+
+    @Test
+    void testFindAll() {
+        assertThat(accountRepository.findAll().size()).isEqualTo(6);
     }
 }
